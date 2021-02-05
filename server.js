3f8e4420bb925e527237b78fbf8c85d0ac4c55e9
renanaribeiro91 <renan.admribeiro@gmail.com>
2021-02-05 13:26:16 -0300

    subindo banco sql

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use()
+
+
+
+
+
+
+
+
+const port = 8000
+app.listen(port,()=>{
+    console.log('Server está on : 8000')
+})
---
+++ src/config_database/database.js
@@ -0,0 +1,11 @@
+module.exports =  {
+    dialect:'postgres',
+    host:"localhost",
+    usarname:"elephantSQL",
+    password:'sqlnode',
+    define:{
+        timestamps:true,  
+        underscored:true  // formato snake_case
+    },
+
+}
